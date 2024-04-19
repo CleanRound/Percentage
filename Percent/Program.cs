6dd4842e6b489265868ef23f2cf7491e022c73b5
Олег Луцик <oleglutsik.dev@gmail.com>
2024-04-19 19:15:39 +0300

    Добавьте файлы проекта.

---
+++ Percent/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the value: ");
+        if (double.TryParse(Console.ReadLine(), out double value))
+        {
+            Console.WriteLine("Enter the percentage: ");
+            if (double.TryParse(Console.ReadLine(), out double percentage))
+            {
+                double result = (percentage / 100) * value;
+                Console.WriteLine($"Result: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Error: Invalid input for percentage.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error: Invalid input for value.");
+        }
+    }
+}
